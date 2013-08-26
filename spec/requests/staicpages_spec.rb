cec118400f989a184228d2932c2e0ae44228f7b8
sherif <sherif_bobos2009@hotmail.com>
2013-08-26 11:15:55 +0300

    fix some email regex bugs

--- spec/requests/staicpages_spec.rb
+++ spec/requests/staicpages_spec.rb
@@ -6,37 +6,28 @@ describe "Staic pages" do
 		describe "Home page" do
 
 
-			before {visit root_path}
-					it {should have_selector('h1' , text: 'Welcome To The Sample App')}
-			end
-					it {should have_selector('title', text: "Ruby On Rails | Home")}
-			end
-			end
-					describe "Help page" do
-						before {visit help_path}
-							it {should have_selector('h1' , text: 'Help')}
-			end				it {should have_selector('title' , text: "Ruby On Rails | Help")}
-			
+				before {visit root_path}
+				it {should have_selector('h1' , text: 'Welcome To The Sample App')}
+				it {should have_selector('title', text: "Ruby On Rails | Home")}
+				end
+						
+				
+				describe "Help page" do
+				before {visit help_path}
+				it {should have_selector('h1' , text: 'Help')}
+				it {should have_selector('title' , text: "Ruby On Rails | Help")}
+				end
 
-			end
-			end
-					describe "About Page" do
-						before{visit about_path}
-
-					it {should have_selector('h1' , text: 'About Us')}
-			end	
-					it {should have_selector('title' , text: "Ruby On Rails | About Us")}
-			
-			end
-			end
-					describe "Contact Page" do
-						before{visit contact_path}
-
-					it {should have_selector('h1' , text: 'Contact')}
-			end	
-					it {should have_selector('title', text: "Ruby On Rails | Contact")}
-					
-			
-			end
-			end
+				
+				describe "About Page" do
+				before{visit about_path}
+				it {should have_selector('h1' , text: 'About Us')}
+				it {should have_selector('title' , text: "Ruby On Rails | About Us")}
+				end
+				
+				describe "Contact Page" do
+				before{visit contact_path}
+				it {should have_selector('h1' , text: 'Contact')}
+				it {should have_selector('title', text: "Ruby On Rails | Contact")}
+				end	
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,10 +5,9 @@ describe "User Pages" do
   
   subject{page}
 
-  discribe "signup page" do 
+  describe "signup page" do 
   	before {visit signup_path}
   	it{should have_selector('h1' , text: 'Sign Up')}
   	it{should have_selector('title' , text: "Ruby On Rails | Sign Up")}
 end
-
 end
